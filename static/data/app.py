f4780c54118ae1080b8d6908bab3f727fb3ad50b
Christopher Janssen <christopherjanssen@MacBook-Pro.local>
2020-01-16 18:59:16 -0800

    worked on app.py, defining new app routes

--- static/data/app.py
+++ static/data/app.py
@@ -4,8 +4,7 @@ import sqlalchemy as db
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
-from flask import Flask
+from flask import Flask, import request
 import simplejson as json
 import decimal, datetime
 
@@ -64,41 +63,15 @@ def all_accidents():
     query = db.select([accident_table])
     ResultProxy = connection.execute(query)
     result = ResultProxy.fetchall()
-    #ResultSet[:3]
-    # Convert list of tuples into normal list
-    #all_accidents = list(np.ravel(ResultSet))
-
-    
-
-
-    
-
-    # #print(result)
-    #return jsonify({'result': [dict(row) for row in result]})
     return json.dumps([dict(r) for r in result], default=alchemyencoder)
 
+@app.route("/api/v1.0/map_data")
+def map_data():
+    connection = engine.connect()
+    query = db.select([accident_table])
+    ResultProxy = connection.execute(query)
+    result = ResultProxy.fetchall()
 
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
 
 
 if __name__ == '__main__':
