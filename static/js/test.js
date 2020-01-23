9ddd4e7db813355e8155e01ea1ff80fc63db9c03
Christopher Janssen <christopherjanssen@MacBook-Pro.local>
2020-01-22 21:32:14 -0800

    updated map js

--- static/js/test.js
+++ static/js/test.js
@@ -1,6 +1,8 @@
+//modified 
 // Store our API endpoint inside queryUrl
 //var url = "/static/data/data.csv";
 var url = "http://127.0.0.1:5000/api/v1.0/map_data";
+url_all = "http://127.0.0.1:5000/api/v1.0/all_accidents";
 console.log(url);
 
 //create an empty array that will hold arrays of the lat, lng, and intensity for each earthquake 
@@ -9,20 +11,79 @@ var heatpoints = []
 // Create a new marker cluster group
 var markers = L.markerClusterGroup();
 
-
-// Perform a GET request to the query URL
+// somefunction();
+//Perform a GET request to the query URL
 d3.json(url, function (data) {
     // Once we get a response, send the data.features object to the createFeatures function
     //console.log(data);
-    console.log("test")
+    console.log("loading all cities")
     createFeatures(data);
+    //populatetable(data);
     
+});
+
+//INPUT #1
+//create city input field reference
+
+
+//this function pans the map to the specific city
+function center_map(city)
+{
+    if(city=="San Jose")
+    {
+        map.panTo(new L.LatLng(37.3382, -121.8863));
+    }
+
+    if(city=="Oakland")
+    {
+        map.panTo(new L.LatLng(37.8044, -122.2712));
+    }
+    if(city=="Sacramento")
+    {
+        map.panTo(new L.LatLng(38.5816, -121.4944));
+    }
+
+    if(city=="Los Angeles")
+    {
+        map.panTo(new L.LatLng(34.0522, -118.2437));
+    }
+    if(city=="San Diego")
+    {
+        map.panTo(new L.LatLng(32.7157, -117.1611));
+    }
+
+
+    if(city=="San Antonio")
+    {
+        map.panTo(new L.LatLng(29.4241, -98.4936));
+    }
+
+    if(city=="Austin")
+    {
+        map.panTo(new L.LatLng(30.2672, -97.7431));
+    }
+
+    if(city=="Houston")
+    {
+        map.panTo(new L.LatLng(29.7604, -95.3698));
+    }
+
+    if(city=="Fort Worth")
+    {
+        map.panTo(new L.LatLng(32.7555, -97.3308));
+    }
+}
+
+
+d3.json(url_all, function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    populatetable(data);
 });
 
 function createFeatures(accidentData) {
     // Pull the "stations" property off of response.data
     var accidentMarkers = [];
-    console.log(accidentData["Start_Time"]);
+    //console.log(accidentData["Start_Time"]);
     
 
     for (var index = 0; index < accidentData.length; index++) {
@@ -46,7 +107,7 @@ function createFeatures(accidentData) {
 
 
 
-function createMap(accidentMarkers) {
+function createMap(accidentMarkers, center_coord) {
 
     // Create the tile layer that will be the background of our map
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
@@ -68,8 +129,8 @@ function createMap(accidentMarkers) {
     };
 
     // Create the map object with options
-    var map = L.map("map", {
-        center: [37.7749, -122.4194],
+    window.map = L.map("map", {
+        center: [32.7555, -97.3308],
         zoom: 12,
         //by default, we only use marker groups
         layers: [lightmap, markers]
@@ -81,7 +142,62 @@ function createMap(accidentMarkers) {
     }).addTo(map);
 }
 
+function somefunction() {
+    element = document.getElementById('city');
+    console.log(element);
+    if (element != null) 
+    {
+        var chosenValue = d3.select('city').property('value');
+        // console.log(chosenValue);
+        url = "localhost:5000/submitted/";
+        d3.json(url, function(d) 
+        {
+            createFeatures(d);
+            console.log(d);
+        })
+    }    
+    else
+    {
+        console.log("bad data");
+    }
+}
+
+function populatetable(filtered)
+    {
+            // Get a reference to the table body
+            var tbody = d3.select("tbody");
+
+            //remove old data
+            rows = tbody.selectAll("tr").remove();
+
+            //itereate through filtered data
+            filtered.forEach(function(ufoSights) {
+                //console.log(ufoSights);
+                var row = tbody.append("tr");
+                Object.entries(ufoSights).forEach(function([key, value]) 
+                {
+                    //console.log(key, value);
+                    // Append a cell to the row for each value
+                    // in the weather report object
+                    var cell = row.append("td");
+                    cell.text(value);
+    			});
+    		});
+
+    }
 
 
+    function handleClick()
+    {
+        console.log("Clicked");
+        event.preventDefault();
+        var chosenValue = d3.select('#city').node().value;
+        console.log(chosenValue);
+        center_map(chosenValue);
 
+    
+    
+    }
+    var button = d3.select("#button");
+    button.on("click", handleClick);
 
