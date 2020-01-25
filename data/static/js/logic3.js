dc30966fc2cbf4ec8580245e8d6ccf2f33a3da85
kdol2495 <53966704+kdol2495@users.noreply.github.com>
2020-01-25 10:49:03 -0800

    editing charts display

---
+++ data/static/js/logic3.js
@@ -0,0 +1,36 @@
+//chart.js plotting 
+var url = "http://127.0.0.1:5000/api/v1.0/city_bar";
+
+var MeSeContext = document.getElementById("myChart3").getContext("2d");
+
+d3.json(url, function (d) {
+    console.log('data', d);
+    // console.log("hello");
+
+    var test_var = {
+        'data': d.map(e => e['Count(city)'])
+    };
+    console.log('test', test_var.data[0]);
+
+    var chart = new Chart(MeSeContext, {
+        type: 'horizontalBar',
+        data: {
+          labels: ["Austin", "Dallas", "Fort Worth", "Houston", "Los Angeles",
+          "Oakland", "Sacramento", "San Antonio", "San Diego", "San Jose"],
+          datasets: [
+            {
+              label: "City",
+              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+              data: test_var.data
+            }
+          ]
+        },
+        options: {
+          legend: { display: true },
+          title: {
+            display: true,
+            text: 'Number of Accidents by City'
+          }
+        }
+    });
+}); 
